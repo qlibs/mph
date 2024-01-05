195c065de93f4d120e4391b4520a0169a30a78cf
Kris Jusiak <kris@jusiak.net>
2024-01-05 13:50:18 -0600

    :lipstick: clang-format

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -10,9 +10,8 @@
 
 #include <algorithm>
 #include <array>
-#include <random>
-
 #include <mph>
+#include <random>
 #include <unordered_map>
 
 #include "data.hpp"
@@ -34,28 +33,22 @@ int main() {
 
   // const auto first = [](const auto &symbols) { return symbols[0]; };
 
-  auto next = [&, i = 0](const auto &symbols) mutable {
-    return symbols[ids[i++ % iterations]];
-  };
+  auto next = [&, i = 0](const auto &symbols) mutable { return symbols[ids[i++ % iterations]]; };
 
-  const auto bench_unordered_map = [](const auto name, const auto &symbols,
-                                      auto fn) {
+  const auto bench_unordered_map = [](const auto name, const auto &symbols, auto fn) {
     std::unordered_map<std::string_view, int> hash_map{};
     for (auto index = 0; const auto &symbol : symbols) {
       hash_map[symbol] = index++;
     }
-    Bench()
-        .minEpochIterations(iterations)
-        .run(std::string(name) + ".unordered_map",
-             [&] { doNotOptimizeAway(hash_map[fn(symbols)]); });
+    Bench().minEpochIterations(iterations).run(std::string(name) + ".unordered_map", [&] {
+      doNotOptimizeAway(hash_map[fn(symbols)]);
+    });
   };
 
-  const auto bench_frozen = [](const auto name, const auto &symbols,
-                               const auto &frozen, auto fn) {
-    Bench()
-        .minEpochIterations(iterations)
-        .run(std::string(name) + ".frozen",
-             [&] { doNotOptimizeAway(frozen.at(fn(symbols))); });
+  const auto bench_frozen = [](const auto name, const auto &symbols, const auto &frozen, auto fn) {
+    Bench().minEpochIterations(iterations).run(std::string(name) + ".frozen", [&] {
+      doNotOptimizeAway(frozen.at(fn(symbols)));
+    });
   };
 
   const auto bench_bsearch = [](const auto name, const auto &symbols, auto fn) {
@@ -65,18 +58,14 @@ int main() {
     }
     std::sort(std::begin(strings), std::end(strings));
     constexpr auto bsearch = [](const auto &symbols, const auto data) -> int {
-      if (const bool found = std::binary_search(std::cbegin(symbols),
-                                                std::cend(symbols), data)) {
-        return std::distance(
-            std::cbegin(symbols),
-            std::lower_bound(std::cbegin(symbols), std::cend(symbols), data));
+      if (const bool found = std::binary_search(std::cbegin(symbols), std::cend(symbols), data)) {
+        return std::distance(std::cbegin(symbols), std::lower_bound(std::cbegin(symbols), std::cend(symbols), data));
       }
       return -1;
     };
-    Bench()
-        .minEpochIterations(iterations)
-        .run(std::string(name) + ".bsearch",
-             [&] { doNotOptimizeAway(bsearch(strings, fn(symbols))); });
+    Bench().minEpochIterations(iterations).run(std::string(name) + ".bsearch", [&] {
+      doNotOptimizeAway(bsearch(strings, fn(symbols)));
+    });
   };
 
   const auto bench_gperf = [](const auto name, const auto &symbols, auto fn) {
@@ -84,50 +73,44 @@ int main() {
     /* Command-line: gperf -e ' \\015' -L C++ -7 -C -E -k '*,1,$' */
     const auto hash = [](const char *str, std::size_t len) {
       static constexpr const unsigned short asso_values[] = {
-          393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393,
-          393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393,
-          393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393,
-          393, 393, 393, 393, 393, 393, 5,   10,  393, 393, 393, 393, 393,
-          393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393,
-          20,  15,  10,  80,  0,   20,  55,  70,  5,   2,   2,   115, 50,
-          15,  105, 50,  55,  0,   5,   0,   2,   115, 0,   90,  2,   60,
-          393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393,
-          393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393,
-          393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393};
+          393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393,
+          393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393,
+          393, 5,   10,  393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 20,
+          15,  10,  80,  0,   20,  55,  70,  5,   2,   2,   115, 50,  15,  105, 50,  55,  0,   5,   0,   2,   115, 0,
+          90,  2,   60,  393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393,
+          393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393, 393};
       unsigned int hval = len;
 
       switch (hval) {
-      default:
-        hval += asso_values[static_cast<unsigned char>(str[5])];
-      /*FALLTHROUGH*/
-      case 5:
-        hval += asso_values[static_cast<unsigned char>(str[4])];
-      /*FALLTHROUGH*/
-      case 4:
-        hval += asso_values[static_cast<unsigned char>(str[3])];
-      /*FALLTHROUGH*/
-      case 3:
-        hval += asso_values[static_cast<unsigned char>(str[2])];
-      /*FALLTHROUGH*/
-      case 2:
-        hval += asso_values[static_cast<unsigned char>(str[1])];
-      /*FALLTHROUGH*/
-      case 1:
-        hval += asso_values[static_cast<unsigned char>(str[0])];
-        break;
+        default:
+          hval += asso_values[static_cast<unsigned char>(str[5])];
+        /*FALLTHROUGH*/
+        case 5:
+          hval += asso_values[static_cast<unsigned char>(str[4])];
+        /*FALLTHROUGH*/
+        case 4:
+          hval += asso_values[static_cast<unsigned char>(str[3])];
+        /*FALLTHROUGH*/
+        case 3:
+          hval += asso_values[static_cast<unsigned char>(str[2])];
+        /*FALLTHROUGH*/
+        case 2:
+          hval += asso_values[static_cast<unsigned char>(str[1])];
+        /*FALLTHROUGH*/
+        case 1:
+          hval += asso_values[static_cast<unsigned char>(str[0])];
+          break;
       }
       return hval;
     };
 
-    Bench()
-        .minEpochIterations(iterations)
-        .run(std::string(name) + ".gperf",
-             [&] { doNotOptimizeAway(hash(fn(symbols).data(), size)); });
+    Bench().minEpochIterations(iterations).run(std::string(name) + ".gperf", [&] {
+      doNotOptimizeAway(hash(fn(symbols).data(), size));
+    });
   };
 
-  const auto bench_mph =
-      [](auto Symbols, const auto name, const auto &symbols, auto fn) {
-      constexpr auto hash = mph::hash{std::move(Symbols)};
+  const auto bench_mph = [](auto Symbols, const auto name, const auto &symbols, auto fn) {
+    constexpr auto hash = mph::hash{std::move(Symbols)};
     Bench().minEpochIterations(iterations).run(std::string(name) + ".mph", [&] {
       doNotOptimizeAway(hash(fn(symbols).data()));
     });
