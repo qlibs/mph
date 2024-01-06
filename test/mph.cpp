92188b6bb06d0a3b9d6ebc8edb2e7aa291702fdb
Kris Jusiak <kris@jusiak.net>
2024-01-06 11:26:56 -0600

    :new: support for fail cases

--- test/mph.cpp
+++ test/mph.cpp
@@ -50,30 +50,30 @@ int main() {
     expect(0_u == hash("b"sv));
   };
 
-  //""_test = [] {
-    //static constexpr std::array symbols{
-        //"A"sv,
-        //"B"sv,
-        //"C"sv,
-    //};
-
-    //constexpr auto hash = mph::hash{[] { return symbols; }, [](auto&&... args) { return mph::pext_split_on_first_char<5>{}(args...); }};
-
-    //for (auto expected = 1u; const auto &symbol : symbols) {
-      //expect(_u(expected++) == hash(symbol));
-    //}
-
-    //expect(0_u == hash(""));
-    //expect(0_u == hash("D"));
-    //expect(0_u == hash("a"));
-    //expect(0_u == hash("b"));
-  //};
 
   ""_test = [] {
     static constexpr std::array symbols{
         "A"sv,
         "B"sv,
         "C"sv,
+    };
+
+    constexpr auto hash = mph::hash{[] { return symbols; }, [](auto&&... args) { return mph::pext_split_on_first_char<5>{}(args...); }};
+
+    for (auto expected = 1u; const auto &symbol : symbols) {
+      expect(_u(expected++) == hash(symbol));
+    }
+
+    expect(0_u == hash("D"sv));
+    expect(0_u == hash("a"sv));
+    expect(0_u == hash("b"sv));
+  };
+
+  ""_test = [] {
+    static constexpr std::array symbols{
+        "A       "sv,
+        "B       "sv,
+        "C       "sv,
     };
 
     auto hash = mph::hash{[] { return symbols; }};
@@ -84,8 +84,8 @@ int main() {
 
     expect(0_u == hash("        ")); // no size information
     expect(0_u == hash("D       ")); // no size information
-    expect(0_u == hash("a       ")); // no size information
-    expect(0_u == hash("b       ")); // no size information
+    expect(0_u == hash("E       ")); // no size information
+    expect(0_u == hash("F       ")); // no size information
   };
 
   ""_test = [] {
@@ -132,36 +132,31 @@ int main() {
     expect(0_u == hash("_AA_"sv));
   };
 
-  //""_test = [] {
-    //static constexpr std::array<std::string_view, 100> symbols{
-      //"III     ", "AGM-C   ", "LOPE    ", "FEMS    ", "IEA     ", "VYMI    ", "BHK     ", "SIEB    ", "DGBP    ", "INFN    ",
-      //"USRT    ", "BCOR    ", "TWM     ", "BVSN    ", "STBA    ", "GPK     ", "LVHD    ", "FTEK    ", "GLBS    ", "CUBB    ",
-      //"LRCX    ", "HTGM    ", "RYN     ", "IPG     ", "PNNTG   ", "ZIG     ", "IVR-A   ", "INVA    ", "MNE     ", "KRA     ",
-      //"BRMK    ", "ARKG    ", "FFR     ", "QTRX    ", "XTN     ", "BAC-A   ", "CYBE    ", "ETJ     ", "JHCS    ", "RBCAA   ",
-      //"GDS     ", "WTID    ", "TCO     ", "BWA     ", "MIE     ", "GENY    ", "TDOC    ", "MCRO    ", "QFIN    ", "NBTB    ",
-      //"PWC     ", "FQAL    ", "NJAN    ", "IWB     ", "GXGXW   ", "EDUC    ", "RETL    ", "VIACA   ", "KLDO    ", "NEE-I   ",
-      //"FBC     ", "JW.A    ", "BSMX    ", "FMNB    ", "EXR     ", "TAC     ", "FDL     ", "SWIR    ", "CLWT    ", "LMHB    ",
-      //"IRTC    ", "CDMO    ", "HMLP-A  ", "LVUS    ", "UMRX    ", "GJH     ", "FRME    ", "CEIX    ", "IHD     ", "GHSI    ",
-      //"DCP-B   ", "SB      ", "DSE     ", "CPRT    ", "NRZ     ", "VLYPO   ", "TDAC    ", "ZXZZT   ", "IWX     ", "NCSM    ",
-      //"WIRE    ", "SFST    ", "EWD     ", "DEACW   ", "TRPX    ", "UCTT    ", "ZAZZT   ", "CYD     ", "NURE    ", "WEAT    ",
-    //};
+  ""_test = [] {
+    static constexpr std::array<std::string_view, 100> symbols{
+      "III     ", "AGM-C   ", "LOPE    ", "FEMS    ", "IEA     ", "VYMI    ", "BHK     ", "SIEB    ", "DGBP    ", "INFN    ",
+      "USRT    ", "BCOR    ", "TWM     ", "BVSN    ", "STBA    ", "GPK     ", "LVHD    ", "FTEK    ", "GLBS    ", "CUBB    ",
+      "LRCX    ", "HTGM    ", "RYN     ", "IPG     ", "PNNTG   ", "ZIG     ", "IVR-A   ", "INVA    ", "MNE     ", "KRA     ",
+      "BRMK    ", "ARKG    ", "FFR     ", "QTRX    ", "XTN     ", "BAC-A   ", "CYBE    ", "ETJ     ", "JHCS    ", "RBCAA   ",
+      "GDS     ", "WTID    ", "TCO     ", "BWA     ", "MIE     ", "GENY    ", "TDOC    ", "MCRO    ", "QFIN    ", "NBTB    ",
+      "PWC     ", "FQAL    ", "NJAN    ", "IWB     ", "GXGXW   ", "EDUC    ", "RETL    ", "VIACA   ", "KLDO    ", "NEE-I   ",
+      "FBC     ", "JW.A    ", "BSMX    ", "FMNB    ", "EXR     ", "TAC     ", "FDL     ", "SWIR    ", "CLWT    ", "LMHB    ",
+      "IRTC    ", "CDMO    ", "HMLP-A  ", "LVUS    ", "UMRX    ", "GJH     ", "FRME    ", "CEIX    ", "IHD     ", "GHSI    ",
+      "DCP-B   ", "SB      ", "DSE     ", "CPRT    ", "NRZ     ", "VLYPO   ", "TDAC    ", "ZXZZT   ", "IWX     ", "NCSM    ",
+      "WIRE    ", "SFST    ", "EWD     ", "DEACW   ", "TRPX    ", "UCTT    ", "ZAZZT   ", "CYD     ", "NURE    ", "WEAT    ",
+    };
 
-    //auto hash = mph::hash{[] { return symbols; }};
+    const auto hash = mph::hash{[] { return symbols; }};
 
-    //auto expected = 1u;
-    //for (const auto &symbol : symbols) {
-      //expect(_u(expected++) == hash(symbol.data()));
-    //}
+    for (auto expected = 1u; const auto &symbol : symbols) {
+      expect(_u(expected++) == hash(symbol.data()));
+    }
 
-    //expect(0_u == hash(""sv));
-    //expect(0_u == hash("        "sv));
-    //expect(0_u == hash("III "sv));
-    //expect(0_u == hash("III  "sv));
-    //expect(0_u == hash("III   "sv));
-    //expect(0_u == hash("III    "sv));
-    //expect(0_u == hash("III     "sv));
-    //expect(0_u == hash(" III    "sv));
-    //expect(0_u == hash("IIIx    "sv));
-    //expect(0_u == hash("     III"sv));
-  //};
+    expect(0_u == hash("        "sv));
+    expect(0_u == hash(" III    "sv));
+    expect(0_u == hash("  III   "sv));
+    expect(0_u == hash("   III  "sv));
+    expect(0_u == hash("    III "sv));
+    expect(0_u == hash("     III"sv));
+  };
 }
