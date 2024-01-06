40b90f2ce4900fa9061343e1938c0d0c5002ac6a
Kris Jusiak <kris@jusiak.net>
2024-01-06 05:42:05 -0600

    :new: example

---
+++ example/mph.cpp
@@ -0,0 +1,29 @@
+//
+// Copyright (c) 2024 Kris Jusiak (kris at jusiak dot net)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+#include <mph>
+#include <string_view>
+#include <array>
+
+int main() {
+  using std::literals::operator""sv;
+
+  constexpr std::array symbols{
+    "FBC"sv,
+    "SPY"sv,
+    "CDC"sv,
+  };
+
+  auto hash = mph::hash{[] { return symbols; }};
+
+  std::cout << hash("");    // 0
+  std::cout << hash("FOO"); // 0
+  std::cout << hash("FOO"); // 0
+  std::cout << hash("FBC"); // 1
+  std::cout << hash("SPY"); // 2
+  std::cout << hash("CDC"); // 3
+}
